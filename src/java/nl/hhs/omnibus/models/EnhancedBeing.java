9d3969119889f4b4250610752dd0645ab1db734d
Oscar Wellner <oscar.wellner@gmail.com>
2022-04-19 18:48:36 +0200

    Remove duplicate and unnecessary code already existing in Identifiable

--- src/java/nl/hhs/omnibus/models/EnhancedBeing.java
+++ src/java/nl/hhs/omnibus/models/EnhancedBeing.java
@@ -1,20 +1,9 @@
 package java.nl.hhs.omnibus.models;
 
-import java.nl.hhs.omnibus.interfaces.Identifiable;
-import java.util.Objects;
-
 /**
  * The EnhancedBeing is the Base class for the Hero and Villain classes.
  */
-public class EnhancedBeing implements Identifiable {
-    private static int nextId = 0;
-
-    /** An internal identifier which is used in the system to identify a unique Hero or Villain. */
-    private int id;
-
-    /** The name of an individual Hero or Villain. */
-    private String name;
-
+public class EnhancedBeing extends Nameable {
     /** The location of which an individual Hero or Villain is known for to be most active in. */
     private String mostActiveLocation;
 
@@ -25,55 +14,19 @@ public class EnhancedBeing implements Identifiable {
     private String phrase;
 
     public EnhancedBeing(String name, String mostActiveLocation, int powerLevel, String phrase) {
-        this.id = ++EnhancedBeing.nextId;
+        super(name);
 
-        this.name = name;
         this.mostActiveLocation = mostActiveLocation;
         this.powerLevel = powerLevel;
         this.phrase = phrase;
     }
 
+    /** Prints the phrase of an EnhancedBeing to the terminal. */
     public void speakPhrase() {
         System.out.println(phrase);
     }
 
-    @Override
-    public boolean equals(Object object) {
-        if (object == null || this.getClass() != object.getClass()) return false;
-        if (this == object) return true;
-
-        EnhancedBeing that = (EnhancedBeing) object;
-
-        return this.id == that.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.id);
-    }
-
-    @Override
-    public String toString() {
-        // TODO: Needs more formatting to be inline with how the details of one EnhancedBeing should be printed
-        return String.format("EnhancedBeing{ id: '%s', name: '%s' }", this.id, this.name);
-    }
-
     /* GETTERS AND SETTERS */
-
-    @Override
-    public int getId() {
-        return this.id;
-    }
-
-    @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public int getPowerLevel() {
         // TODO: Should actually return a calculated value which is the powerLevel of an Hero or Villain +
