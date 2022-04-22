c23719e8cc6e6636b1a1ccc462957cf40eb7b4d8
Oscar Wellner <oscar.wellner@gmail.com>
2022-04-22 20:47:29 +0200

    Add Hero and Villain

--- src/main/java/nl/hhs/omnibus/models/persons/Villain.java
+++ src/main/java/nl/hhs/omnibus/models/persons/Villain.java
@@ -3,24 +3,57 @@ package nl.hhs.omnibus.models.persons;
 import nl.hhs.omnibus.models.EnhancedBeing;
 
 import java.util.HashMap;
-import java.util.HashSet;
-
+import java.util.Map;
 
 public class Villain extends EnhancedBeing {
-    private static final String defaultPhrase = "Oneliner Villain";
-    private String evilPlan;
+    private final String evilPlan;
+    private final Map<Hero, Boolean> rivals = new HashMap<>();
 
-   // private HashMap<Hero, Boolean> rivals = new HashMap<Hero, Boolean>();
-   // private HashSet<VillainTeam> teams = new HashSet<VillainTeam>();
+    public Villain(String name, String mostActiveLocation, int powerLevel, String evilPlan) {
+        super(name, mostActiveLocation, powerLevel, "Oneliner Villain!");
 
-    public Villain(String name, String mostActiveLocation, int powerLevel, String evilPlan, String phrase){
+        this.evilPlan = evilPlan;
+    }
+
+    public Villain(String name, String mostActiveLocation, int powerLevel, String evilPlan, String phrase) {
         super(name, mostActiveLocation, powerLevel, phrase);
-        this.evilPlan = evilPlan;
-    }
-    public Villain(String name, String mostActiveLocation, int powerLevel, String evilPlan){
-        super(name, mostActiveLocation, powerLevel, defaultPhrase);
-        this.evilPlan = evilPlan;
-    }
-    // TODO: Methods need to be created for the class
 
+        this.evilPlan = evilPlan;
+    }
+
+    public void addAllRivals(Map<Hero, Boolean> rivals) {
+        rivals.forEach(this::addRival);
+    }
+
+    public void addRival(Hero rival, boolean isArchRival) {
+        this.rivals.put(rival, isArchRival);
+    }
+
+    /*  Data */
+
+    public static class Villains {
+        public static Villain THE_JOKER = new Villain("The Joker", "Gotham City", -1, "");
+        public static Villain RIDDLER = new Villain("Riddler", "Gotham City", -1, "");
+        public static Villain BLACK_MANTA = new Villain("Black Manta", "Mobile", -1, "");
+        public static Villain DARKSEID = new Villain("Darkseid", "Apokolips", -1, "");
+        public static Villain MISTER_FREEZE = new Villain("Mister Freeze", "Gotham City", -1, "");
+        public static Villain BASILISK = new Villain("Basilisk", "The Masada", -1, "");
+        public static Villain ABRA_KADABRA = new Villain("Abra Kadabra", "Keystone City", -1, "");
+        public static Villain BLACK_RACER = new Villain("Black Racer", "Mobile", -1, "");
+        public static Villain KILLER_MOTH = new Villain("Killer Moth", "Gotham City", -1, "");
+        public static Villain MIDAS = new Villain("Midas", "Seattle", -1, "");
+        public static Villain THE_DROWNED = new Villain("The Drowned", "Amnesty Bay", -1, "");
+        public static Villain DAWNBREAKER = new Villain("Dawnbreaker", "Gotham City", -1, "");
+        public static Villain ZUMBADO = new Villain("Zumbado", "Mobile", -1, "");
+        public static Villain DESPERO = new Villain("Despero", "Mobile", -1, "");
+        public static Villain LEX_LUTHER = new Villain("Lex Luther", "Metropolis", -1, "");
+        public static Villain GRID = new Villain("Grid", "Mobile", -1, "");
+        public static Villain CAPTAIN_COLD = new Villain("Caption Cold", "Centeral City", -1, "");
+        public static Villain CHARNN = new Villain("Cha'rnn O'zzm", "Earth", -1, "");
+        public static Villain HUMAN_FLAME = new Villain("Human Flame", "Metropolis", -1, "");
+        public static Villain DESAAD = new Villain("Desaad", "Apokolips", -1, "");
+        public static Villain STEPPENWOLF = new Villain("Steppenwolf", "Apokolips", -1, "");
+
+        public static Villain[] VILLAINS = { THE_JOKER, RIDDLER, BLACK_MANTA, DARKSEID, MISTER_FREEZE, BASILISK, ABRA_KADABRA, BLACK_RACER, KILLER_MOTH, MIDAS, THE_DROWNED, DAWNBREAKER, ZUMBADO, DESPERO, LEX_LUTHER, GRID, CAPTAIN_COLD, CHARNN, HUMAN_FLAME, DESAAD, STEPPENWOLF };
+    }
 }
