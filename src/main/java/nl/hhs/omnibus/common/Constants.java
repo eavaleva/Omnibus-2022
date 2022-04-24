98657a14d6c9b8993a45f0e7d09c068f9c57b6a7
Oscar Wellner <oscar.wellner@gmail.com>
2022-04-24 23:20:01 +0200

    Reformat full details output

--- src/main/java/nl/hhs/omnibus/common/Constants.java
+++ src/main/java/nl/hhs/omnibus/common/Constants.java
@@ -1,36 +1,39 @@
 package nl.hhs.omnibus.common;
 
 public class Constants {
-    public static final String ID = "ID";
-    public static final String NAME = "Name";
-    public static final String LOCATION = "Location";
-    public static final String POWER_LEVEL = "Power level";
+    public static final String SEPARATOR = "----------------------------------------\n";
+    public static final String ID = "ID:";
+    public static final String NAME = "Name:";
+    public static final String LOCATION = "Location:";
+    public static final String POWER_LEVEL = "Power level:";
 
     public static final String DEFAULT_ONE_LINER = "One-liner %s!";
-    public static final String ONE_LINER = "One-liner";
-    public static final String RIVALS_HEADER = "Rivals";
-    public static final String ARCH_RIVALS_HEADER = "Arch rivals";
-    public static final String REAL_NAME = "Real name";
-    public static final String EVIL_PLAN = "Evil plan";
-    public static final String HERO = "Hero";
-    public static final String VILLAIN = "Villain";
+    public static final String ONE_LINER = "One-liner:";
+    public static final String RIVALS_HEADER = "Rivals:";
+    public static final String ARCH_RIVALS_HEADER = "Arch rivals:";
+    public static final String REAL_NAME = "Real name:";
+    public static final String EVIL_PLAN = "Evil plan:";
+    public static final String HERO = "Hero:";
+    public static final String VILLAIN = "Villain:";
 
-    public static final String OWNER = "Owner";
-    public static final String TYPE = "Type";
-    public static final String SPEED = "Speed";
-    public static final String CAPACITY = "Capacity";
+    public static final String OWNER = "Owner:";
+    public static final String TYPE = "Type:";
+    public static final String SPEED = "Speed:";
+    public static final String CAPACITY = "Capacity:";
 
-    public static final String FAVORITE_CHARACTERS_HEADER = "Fan of";
+    public static final String FAVORITE_CHARACTERS_HEADER = "Fan of:";
 
     public static final String NO_FAVORITE_CHARACTERS = "CURRENTLY DOESN'T FAVOR ANY HERO OR VILLAIN.";
     public static final String NO_RIVALS = "NO RIVALS";
     public static final String NO_ARCH_RIVALS = "NO ARCH RIVALS";
 
     public static final String LAUNCHING_APPLICATION_MESSAGE = "\nWelcome to Omnibus...";
-    public static final String MAKE_SELECTION_MESSAGE = "Please select a an option [0-%d]: ";
+    public static final String MAKE_OPTION_SELECTION_MESSAGE = "Please select an option [0-%d]: ";
+    public static final String MAKE_LIST_SELECTION_MESSAGE = "\nPlease select an item from the list by providing the ID or the name of the item: ";
     public static final String CLOSING_APPLICATION_MESSAGE = "\nClosing Omnibus...";
 
     public static final String SELECTION_OUTSIDE_RANGE_EXCEPTION_MESSAGE = "\tYour selection (%d) is outside of the accepted range. Please, make another selection within the following range [0-%d]\n\n";
 
-    public static final String NO_ITEMS = "NO ITEMS TO SHOW.";
+    public static final String NO_ITEMS = "\tNO ITEMS TO SHOW.\n";
+    public static final String NO_SELECTION_RESULT = "\tNO ITEM WAS FOUND BY '%s': %s\n";
 }
--- src/main/java/nl/hhs/omnibus/models/persons/Hero.java
+++ src/main/java/nl/hhs/omnibus/models/persons/Hero.java
@@ -29,14 +29,14 @@ public class Hero extends EnhancedBeing {
 
     @Override
     public String toString() {
-        StringBuilder details = new StringBuilder(String.format("%14s#%03d\n", Constants.ID, this.getId()));
-        details.append(String.format("%14s:%s\n", Constants.HERO, this.getName()));
-        details.append(String.format("%14s:%s\n", Constants.LOCATION, this.getMostActiveLocation()));
-        details.append(String.format("%14s:%s\n", Constants.REAL_NAME, this.realName));
-        details.append(String.format("\n%14s:\"%s\"\n", Constants.ONE_LINER, this.getPhrase()));
+        StringBuilder details = new StringBuilder(String.format("%s%-14s#%03d\n", Constants.SEPARATOR, Constants.ID, this.getId()));
+        details.append(String.format("%-14s%s\n", Constants.HERO, this.getName()));
+        details.append(String.format("%-14s%s\n", Constants.LOCATION, this.getMostActiveLocation()));
+        details.append(String.format("%-14s%s\n", Constants.REAL_NAME, this.realName));
+        details.append(String.format("\n%-14s\"%s\"\n", Constants.ONE_LINER, this.getPhrase()));
 
-        StringBuilder rivals = new StringBuilder(String.format("\n%s:\n", Constants.RIVALS_HEADER));
-        StringBuilder archRival = new StringBuilder(String.format("\n%s:\n", Constants.ARCH_RIVALS_HEADER));
+        StringBuilder rivals = new StringBuilder(String.format("\n%s\n", Constants.RIVALS_HEADER));
+        StringBuilder archRival = new StringBuilder(String.format("\n%s\n", Constants.ARCH_RIVALS_HEADER));
 
         // Add all rivals and the arch rival to their own lists
         for (Map.Entry<Villain, Boolean> entry : this.rivals.entrySet()) {
@@ -54,20 +54,21 @@ public class Hero extends EnhancedBeing {
         // When a Hero has rivals no
         if (this.rivals.size() == 0) {
             rivals.append(String.format("\t%s\n", Constants.NO_RIVALS));
-            rivals.append(String.format("\t%s\n", Constants.NO_ARCH_RIVALS));
+            archRival.append(String.format("\t%s\n", Constants.NO_ARCH_RIVALS));
         }
 
         // When a Hero has no arch rival
         if (!this.rivals.containsValue(true) && !archRival.toString().contains(Constants.NO_ARCH_RIVALS)) {
-            rivals.append(String.format("\t%s\n", Constants.NO_ARCH_RIVALS));
+            archRival.append(String.format("\t%s\n", Constants.NO_ARCH_RIVALS));
         }
 
         // When a Hero has no regular rivals (but does have an arch rival)
-        if (!this.rivals.containsValue(false) && !archRival.toString().contains(Constants.NO_RIVALS)) {
+        if (!this.rivals.containsValue(false) && !rivals.toString().contains(Constants.NO_RIVALS)) {
             rivals.append(String.format("\t%s\n", Constants.NO_RIVALS));
         }
         details.append(rivals);
         details.append(archRival);
+        details.append(Constants.SEPARATOR);
 
         return details.toString();
     }
