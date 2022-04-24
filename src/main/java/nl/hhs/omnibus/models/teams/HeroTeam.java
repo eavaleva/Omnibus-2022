0170b2c204ffd4cd7d2b803b22242fc8eeeca607
JordyHHS <jordyhuizer@gmail.com>
2022-04-24 14:23:42 +0200

    Created HeroTeam class. Basically a copy paste from VillainTeam with a few different tweaks. Changed Villan to Hero, mainly.

---
+++ src/main/java/nl/hhs/omnibus/models/teams/HeroTeam.java
@@ -0,0 +1,31 @@
+package nl.hhs.omnibus.models.teams;
+
+import nl.hhs.omnibus.Hero;
+import nl.hhs.omnibus.models.Nameable;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class HeroTeam extends Nameable {
+    private HashSet<Hero> members = new HashSet<>();
+
+    public HeroTeam(String name) {
+        super(name);
+    }
+
+
+    public boolean isMemberOf(Hero hero) {
+        return members.contains(hero);
+    }
+
+
+    public void addAllMembers(Hero... members) {
+        this.members.addAll(Arrays.asList(members));
+
+    }
+
+    public void addMember(Hero member) {
+        this.members.add(member);
+    }
+
+}
