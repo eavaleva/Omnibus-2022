1944f61f3a8b512332868d3cb021b3fb0d1d9751
JordyHHS <jordyhuizer@gmail.com>
2022-04-24 15:36:09 +0200

    Left out empty lines and used the right package import this time.

--- src/main/java/nl/hhs/omnibus/models/teams/HeroTeam.java
+++ src/main/java/nl/hhs/omnibus/models/teams/HeroTeam.java
@@ -1,6 +1,6 @@
 package nl.hhs.omnibus.models.teams;
 
-import nl.hhs.omnibus.Hero;
+import nl.hhs.omnibus.persons.Hero;
 import nl.hhs.omnibus.models.Nameable;
 
 import java.util.Arrays;
@@ -13,17 +13,14 @@ public class HeroTeam extends Nameable {
         super(name);
     }
 
-
     public boolean isMemberOf(Hero hero) {
         return members.contains(hero);
     }
-
 
     public void addAllMembers(Hero... members) {
         this.members.addAll(Arrays.asList(members));
 
     }
-
     public void addMember(Hero member) {
         this.members.add(member);
     }
