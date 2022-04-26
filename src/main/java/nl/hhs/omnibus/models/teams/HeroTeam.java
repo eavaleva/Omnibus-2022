09af9383f62749da0168f350ede64b1ec7873bd4
Oscar Wellner <oscar.wellner@gmail.com>
2022-04-26 20:26:50 +0200

    Refactor Teams

--- src/main/java/nl/hhs/omnibus/models/teams/HeroTeam.java
+++ src/main/java/nl/hhs/omnibus/models/teams/HeroTeam.java
@@ -5,24 +5,31 @@ import nl.hhs.omnibus.models.Nameable;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 public class HeroTeam extends Nameable {
-    private HashSet<Hero> members = new HashSet<>();
+    private final Set<Hero> members = new HashSet<>();
 
     public HeroTeam(String name) {
         super(name);
     }
 
-    public boolean isMemberOf(Hero hero) {
-        return members.contains(hero);
+    @Override
+    public String getDetails(boolean getFullDetails) {
+        return null;
     }
+
+    public boolean isMemberOf(Hero hero) {
+        return this.members.contains(hero);
+    }
+
+    /* GETTERS & SETTERS */
 
     public void addAllMembers(Hero... members) {
         this.members.addAll(Arrays.asList(members));
-
     }
+
     public void addMember(Hero member) {
         this.members.add(member);
     }
-
 }
--- src/main/java/nl/hhs/omnibus/models/teams/VillainTeam.java
+++ src/main/java/nl/hhs/omnibus/models/teams/VillainTeam.java
@@ -5,28 +5,31 @@ import nl.hhs.omnibus.models.persons.Villain;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 public class VillainTeam extends Nameable {
-    private HashSet<Villain> members = new HashSet<>();
+    private final Set<Villain> members = new HashSet<>();
 
     public VillainTeam(String name) {
         super(name);
     }
 
-
     public boolean isMemberOf(Villain villain) {
         return members.contains(villain);
     }
 
+    @Override
+    public String getDetails(boolean getFullDetails) {
+        return null;
+    }
 
+    /* GETTER & SETTERS */
 
     public void addAllMembers(Villain... members){
        this.members.addAll(Arrays.asList(members));
-
     }
 
     public void addMember(Villain member){
         this.members.add(member);
     }
-
 }
