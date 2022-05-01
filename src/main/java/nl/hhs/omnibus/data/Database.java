636e6e971aacba1240c3b88dbe5d5fcb1c0eea23
Eleonora <eleonora.avaleva@gmailc.com>
2022-05-01 15:14:47 +0200

    added the rest of the favorite characters to fans, added powerlevel to weapons

--- src/main/java/nl/hhs/omnibus/data/Database.java
+++ src/main/java/nl/hhs/omnibus/data/Database.java
@@ -97,8 +97,8 @@ public class Database {
         Hero wolverine = new Hero("Wolverine", "Alberta", 25, "James Howlett", "Meet my claws!");
         Hero ironMan = new Hero("Iron man", "Long Island", 26, "Tony Stark", "The Truth is… I am Iron Man.");
         Hero thor = new Hero("Thor", "Asgardian", 26, "Thor Borson", "You have no idea what you are dealing with.");
-        Hero hulk = new Hero("Hulk", "Sakaaran", 50, "Robert Bruce Banner", "Hulk will break metal man open like a tin can. Metal man thinks he is stronger than Hulk. Metalman wrong");
-        Hero spiderman = new Hero("Spiderman", "New York", 14, "Peter Parker", "With great power, comes great responsibility");
+        Hero hulk = new Hero("Hulk", "Sakaaran", 50, "Robert Bruce Banner", "Hulk will break metal man open like a tin can. Metal man thinks he is stronger than Hulk. Metalman wrong.");
+        Hero spiderman = new Hero("Spiderman", "New York", 14, "Peter Parker", "With great power, comes great responsibility.");
 
         this.heroes = Arrays.asList(batman, catwoman, blackWidow, wolverine, ironMan, thor, hulk, spiderman);
 
@@ -109,9 +109,9 @@ public class Database {
         this.heroicTeams = Arrays.asList(theAvengers, theFantasticFour);
 
         // Villains
-        Villain joker = new Villain("Joker", "Gotham City", 49, "Selling Smylex", "If you're good at something, never do it for free");
-        Villain mrFreeze = new Villain("Mr. Freeze", "Gotham City", 19, "Destroy everything Batman loves");
-        Villain lokiLaufeyson = new Villain("Loki Laufeyson", "Asgard", 27, "Take over the planet");
+        Villain joker = new Villain("Joker", "Gotham City", 49, "Selling Smylex", "If you're good at something, never do it for free.");
+        Villain mrFreeze = new Villain("Mr. Freeze", "Gotham City", 19, "Destroy everything Batman loves.");
+        Villain lokiLaufeyson = new Villain("Loki Laufeyson", "Asgard", 27, "Take over the planet.");
         Villain abomination = new Villain("The Abomination", "Harlem", 50, "Kill the Hulk! Gain more power in order to be able to become the most powerful warrior on Earth by killing everyone who stands in his way.");
         Villain deadPool = new Villain("Evil Deadpool", "England", 35, "Bring hell to humanity!");
         Villain ladyDeathstrike = new Villain("Lady Deathstrike", "Osaka", 30, "Destroy everyone!");
@@ -139,26 +139,25 @@ public class Database {
         Vehicle f22Raptor = new Vehicle("F-22 Raptor", "A single-seat, twin-engine fifth-generation super maneuverable fighter aircraft that uses stealth technology.", ironMan, "1845 km/h", 1);
         Weapon rocketPropelledGrenade = new Weapon("Rocket propelled grenade", "Click, BOOM!", batman, 75);
         Weapon batarang = new Weapon("Batarang", "Batman's boomerang", batman, 35);
-        Weapon whip = new Weapon("Whip", "Uses her whip to attack in combat, and to trip up enemies. ", catwoman, 0);
-        Weapon caltrop = new Weapon("Caltrop", "A sharp, metal jacks that could be dropped and used as a trap. Enemies trip and fall down when they step on them.", catwoman, 0);
-        Weapon teaserDisks = new Weapon("Teaser Disks", "Teaser disks are an extension of her widow bites, and these disks can electrocute a person without needing to make contact with them.", blackWidow, 0);
-        Weapon dualBatons = new Weapon("Dual Batons", "Similar to her widow bites, Natasha's dual batons are electrically charged, where she is able to electrocute the Ultron bots, taking them offline after only a few precise blows.", blackWidow, 0);
-        Weapon adamantiumClaws = new Weapon("adamantium claws ", "His Claws Are Fused With Adamantium (a rare metal).", wolverine, 0);
-        Weapon healingPowers = new Weapon("healing powers", "The ability to heal others ", wolverine, 0);
-        Weapon flameThrower = new Weapon("Flame thrower", "Create one hot enough and it could have melted some Ultron robots. ", ironMan, 0);
-        Weapon missiles = new Weapon("Missiles", "To kit out his suits with enough firepower to take down a tank or even a bunker", ironMan, 0);
-        Weapon jarnbjorn = new Weapon("Jarnbjorn", "The Battle Axe", thor, 0);
-        Weapon ultimateMjolnir = new Weapon("Ultimate Mjolnir", "the mystical Uru Warhammer.", thor, 0);
-        Weapon utilityBelt = new Weapon("Utility Belt", "A belt", spiderman, 0);
-        Weapon acidFlower = new Weapon("Acid Flower", "Acidic flowers, that kill everyone who touches them. ", joker, 0);
-        Weapon razorCards = new Weapon("Razor cards", "Sharp razor cards", joker, 0);
-        Weapon iceGun = new Weapon("Ice Gun", "Also known simply as the 'Cold Gun' and 'Freeze Ray', is a piece of cryogenics weaponry which serves as Mister Freeze's signature weapon.", mrFreeze, 0);
-        Weapon magicSword = new Weapon("Magic sword", "A magic sword, known in Norse mythology as Laevateinn, which means \"wounding wand\".", lokiLaufeyson, 0);
-        Weapon cyberpathy = new Weapon("Cyberpathy", "Deathstrike is able to cybernetically interface her consciousness with external computer systems, " + "allowing for direct data access to her brain's memory centers and granting her the ability to remotely operate digital & computerized systems.", ladyDeathstrike, 0);
-        Weapon weaponV = new Weapon("Weapon V ", "In addition to wielding military ordinance, Weapon V operatives can use their symbiotes to manifest claws, fangs, spikes, tendrils, and blades.", venom, 0);
-        Weapon pumpkinBombs = new Weapon("Pumpkin Bombs", "Osborn uses these specialized bombs to blow up or disintegrate his targets.", greenGoblin, 0);
-        Weapon razorBats = new Weapon("Razor Bats", "Osborn uses these drones to slice his enemies.", greenGoblin, 0);
-
+        Weapon whip = new Weapon("Whip", "Uses her whip to attack in combat, and to trip up enemies. ", catwoman, 15);
+        Weapon caltrop = new Weapon("Caltrop", "A sharp, metal jacks that could be dropped and used as a trap. Enemies trip and fall down when they step on them.", catwoman, 10);
+        Weapon teaserDisks = new Weapon("Teaser Disks", "Teaser disks are an extension of her widow bites, and these disks can electrocute a person without needing to make contact with them.", blackWidow, 15);
+        Weapon dualBatons = new Weapon("Dual Batons", "Similar to her widow bites, Natasha's dual batons are electrically charged, where she is able to electrocute the Ultron bots, taking them offline after only a few precise blows.", blackWidow, 17);
+        Weapon adamantiumClaws = new Weapon("adamantium claws ", "His Claws Are Fused With Adamantium (a rare metal).", wolverine, 16);
+        Weapon healingPowers = new Weapon("healing powers", "The ability to heal others ", wolverine, 8);
+        Weapon flameThrower = new Weapon("Flame thrower", "Create one hot enough and it could have melted some Ultron robots. ", ironMan, 14);
+        Weapon missiles = new Weapon("Missiles", "To kit out his suits with enough firepower to take down a tank or even a bunker.", ironMan, 7);
+        Weapon jarnbjorn = new Weapon("Jarnbjorn", "The Battle Axe", thor, 10);
+        Weapon ultimateMjolnir = new Weapon("Ultimate Mjolnir", "the mystical Uru Warhammer.", thor, 12);
+        Weapon utilityBelt = new Weapon("Utility Belt", "A belt", spiderman, 8);
+        Weapon acidFlower = new Weapon("Acid Flower", "Acidic flowers, that kill everyone who touches them. ", joker, 12);
+        Weapon razorCards = new Weapon("Razor cards", "Sharp razor cards", joker, 13);
+        Weapon iceGun = new Weapon("Ice Gun", "Also known simply as the 'Cold Gun' and 'Freeze Ray', is a piece of cryogenics weaponry which serves as Mister Freeze's signature weapon.", mrFreeze, 15);
+        Weapon magicSword = new Weapon("Magic sword", "A magic sword, known in Norse mythology as Laevateinn, which means \"wounding wand\".", lokiLaufeyson, 10);
+        Weapon cyberpathy = new Weapon("Cyberpathy", "Deathstrike is able to cybernetically interface her consciousness with external computer systems, " + "allowing for direct data access to her brain's memory centers and granting her the ability to remotely operate digital & computerized systems.", ladyDeathstrike, 9);
+        Weapon weaponV = new Weapon("Weapon V ", "In addition to wielding military ordinance, Weapon V operatives can use their symbiotes to manifest claws, fangs, spikes, tendrils, and blades.", venom, 18);
+        Weapon pumpkinBombs = new Weapon("Pumpkin Bombs", "Osborn uses these specialized bombs to blow up or disintegrate his targets.", greenGoblin, 14);
+        Weapon razorBats = new Weapon("Razor Bats", "Osborn uses these drones to slice his enemies.", greenGoblin, 16);
         this.gadgets = Arrays.asList(
                 batcave,
                 underwaterBase,
@@ -232,6 +231,17 @@ public class Database {
         eleonoraAvaleva.addAllFavorites(ladyDeathstrike,blackWidow,thor,lokiLaufeyson);
         oscarWellner.addAllFavorites(joker,batman,blackWidow, greenGoblin);
         jordyHuizer.addAllFavorites(spiderman,catwoman,batman,joker);
+        milevaMaric.addAllFavorites(batman, catwoman, blackWidow, thor, hulk, spiderman);
+        maryCurieary.addAllFavorites(joker, mrFreeze, lokiLaufeyson, abomination, deadPool, ladyDeathstrike, venom);
+        rogerPenrose.addAllFavorites(abomination, deadPool, ladyDeathstrike, venom, greenGoblin);
+        jamesGosling.addAllFavorites(joker, mrFreeze, blackWidow, abomination);
+        joseSaramago.addAllFavorites(joker, mrFreeze, lokiLaufeyson,wolverine, ironMan);
+        titosPullo.addAllFavorites(abomination, deadPool, ladyDeathstrike, venom, greenGoblin);
+        guidoVanRossum.addAllFavorites(wolverine, ironMan,spiderman);
+        dennisRitchie.addAllFavorites(ironMan,abomination, joker,spiderman);
+        adaLovelace.addAllFavorites(lokiLaufeyson, abomination, deadPool,ironMan);
+        albertCamus.addAllFavorites(lokiLaufeyson, abomination, deadPool, ladyDeathstrike,wolverine);
+        aldusHuxley.addAllFavorites(lokiLaufeyson, ladyDeathstrike, blackWidow);
 
 
         // form a team of heroes
